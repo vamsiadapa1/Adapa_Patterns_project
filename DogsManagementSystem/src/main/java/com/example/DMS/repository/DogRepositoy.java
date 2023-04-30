7e721e07eccf16b94d03ce8b7c1601d6d3773018
Vamsi Krishna Adapa <73695915+vamsiadapa1@users.noreply.github.com>
2023-04-30 09:32:52 +0530

    changes added

---
+++ DogsManagementSystem/src/main/java/com/example/DMS/repository/DogRepositoy.java
@@ -0,0 +1,13 @@
+package com.example.DMS.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.DMS.Models.Dog;
+
+public interface DogRepositoy extends CrudRepository<Dog,Integer> {
+
+	List<Dog> findByName(String name);
+	
+}
